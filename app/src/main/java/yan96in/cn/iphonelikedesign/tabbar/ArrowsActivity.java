63765398c6be0d2d9bfdfe38d232691afddb2360
yan96in <yan96in@163.com>
2015-12-21 15:52:30 +0800

    first commit -tabbar

---
+++ app/src/main/java/yan96in/cn/iphonelikedesign/tabbar/ArrowsActivity.java
@@ -0,0 +1,18 @@
+package yan96in.cn.iphonelikedesign.tabbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import yan96in.cn.iphonelikedesign.R;
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class ArrowsActivity extends Activity {
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arrowspage);
+    }
+}
